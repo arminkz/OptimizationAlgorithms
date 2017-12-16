f139755620007aa3a8682c2079995a50f98164d7
Armin Kazemi <arminkz@live.com>
2017-12-16 15:23:03 +0330

    Added Hill Climbing Strategies

--- src/algorithm/HillClimbing.java
+++ src/algorithm/HillClimbing.java
@@ -2,7 +2,9 @@ package algorithm;
 //Hill Climbing Algorithm
 //Created by Armin
 
+import javafx.util.Pair;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HillClimbing {
 
@@ -22,35 +24,108 @@ public class HillClimbing {
     }
 
 
-    public void solve(OptimizationProblem op,boolean maximize){
+    public void solve(OptimizationProblem op, HillClimbingSterategy strategy ,boolean maximize){
 
         State currentState = op.initialState();
 
         while(true){
 
-            State bestNode = null ;
-            int bestVal = Integer.MIN_VALUE;
-            Action bestAction = null;
-
+            //Get Neighbours
+            ArrayList<Pair<State,Action>> neighbours = new ArrayList<>();
             for(Action act : op.actions(currentState)){
                 for(State target : op.result(currentState,act)) {
-                    int tval = op.eval(target);
-                    if ((maximize && tval > bestVal) || (!maximize && tval < bestVal) || bestNode == null){
-                        bestNode = target;
-                        bestVal = tval;
-                        bestAction = act;
-                    }
+                    neighbours.add(new Pair(target,act));
                 }
             }
 
-            if(bestNode == null){
-                //no better node exists final node is current node
-                finalState = currentState;
-                return;
+            if(strategy == HillClimbingSterategy.SIMPLE){
+
+                State bestNode = null ;
+                int bestVal = Integer.MIN_VALUE;
+                Action bestAction = null;
+
+                for(Pair<State,Action> psa : neighbours){
+                    int tval = op.eval(psa.getKey());
+                    if ((maximize && tval > bestVal) || (!maximize && tval < bestVal) || bestNode == null){
+                        bestNode = psa.getKey();
+                        bestVal = tval;
+                        bestAction = psa.getValue();
+                    }
+                }
+
+                if(bestNode == null){
+                    //no better node exists final node is current node
+                    finalState = currentState;
+                    return;
+                }
+
+                currentState = bestNode;
+                solution.add(bestAction);
+
+            }else if(strategy == HillClimbingSterategy.FIRST_CHOICE){
+
+                int curval = op.eval(currentState);
+                boolean isFound = false;
+
+                Random rnd = new Random();
+                while(neighbours.size() > 0){
+                    int index = rnd.nextInt(neighbours.size());
+                    Pair<State,Action> psa = neighbours.get(index);
+
+                    int tval = op.eval(psa.getKey());
+                    if (tval > curval){
+                        //Choose First Better State
+                        currentState = psa.getKey();
+                        solution.add(psa.getValue());
+                        isFound = true;
+                        break;
+                    }
+
+                    //remove visited
+                    neighbours.remove(index);
+                }
+
+                if(!isFound){
+                    //No Better State Found Declare as final
+                    finalState = currentState;
+                    return;
+                }
+
+            }else if(strategy == HillClimbingSterategy.STOCHASTIC){
+
+                ArrayList<Pair<State,Action>> betterStates = new ArrayList<>();
+
+                int curval = op.eval(currentState);
+                boolean isFound = false;
+
+                for(Pair<State,Action> psa : neighbours){
+                    int tval = op.eval(psa.getKey());
+                    if (tval > curval){
+                        //Add Better State to Array
+                        betterStates.add(psa);
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if(!isFound){
+                    //No Better State Found Declare as final
+                    finalState = currentState;
+                    return;
+                }else{
+                    Random rnd = new Random();
+                    int index = rnd.nextInt(betterStates.size());
+                    Pair<State,Action> s = betterStates.get(index);
+                    currentState = s.getKey();
+                    solution.add(s.getValue());
+                }
+
+            }else{
+
+                System.err.println("Invalid Strategy !");
+
             }
 
-            currentState = bestNode;
-            solution.add(bestAction);
 
         }
 
---
+++ src/algorithm/HillClimbingSterategy.java
@@ -0,0 +1,7 @@
+package algorithm;
+
+public enum HillClimbingSterategy{
+    SIMPLE,
+    STOCHASTIC,
+    FIRST_CHOICE
+}
